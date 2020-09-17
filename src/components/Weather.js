ad856ddcdb1381da280cc6c4898548cb3f5129ac
TomiMalmberg <69836677+TomiMalmberg@users.noreply.github.com>
2020-09-17 09:11:21 +0300

    17.9.2020

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import {VictoryChart, VictoryLine, VictoryBar, VictoryTheme, VictoryLabel, VictoryScatter, VictoryVoronoiContainer} from 'victory'
+import React, { useState } from 'react'
+import {VictoryChart, VictoryLine, VictoryBar, VictoryTheme, VictoryScatter, VictoryVoronoiContainer} from 'victory'
 
 function Weather() {
   
@@ -13,8 +13,37 @@ function Weather() {
 
     ]
     
+    const today = new Date();
+    const date = today.getDate() + "." + parseInt(today.getMonth()+1) + "." + today.getFullYear();
+
+    const initWeather = [];
+    const [weather, setWeather] = useState(initWeather);
+
+    fetch('https://funcvariaiot.azurewebsites.net/api/HttpTriggerGetIotData?code=qO5qkShg0osHqY0BB2nfXI/anPgQ/K/3mIF7VTCFfaTdrvo6wl6DKw==')
+      .then(response => response.json())
+      .then(json => setWeather([...json]));
+
+      const rows = () => weather.slice(0, 24).reverse().map(temphum => {
+          const measurementDate = temphum.PublishedAt.split('T')[0].split('-')[2] + '.' + temphum.PublishedAt.split('T')[0].split('-')[1] + '.' + temphum.PublishedAt.split('T')[0].split('-')[0]
+          const measurementTime = temphum.PublishedAt.split('T')[1].split(':')[1] + ':' + temphum.PublishedAt.split('T')[1].split(':')[1]
+          return <div> <b>Pvm: </b> {measurementDate}, <b>klo:</b> {measurementTime}--------<b>Ilmankosteus:</b> {temphum.Hum.split('.')[0]}%--------<b>Lämpötila:</b> {temphum.Temp.split('.')[0]}</div>
+      })
+
     return (
-        <div aling="middle">
+        <div aling="center">
+            <div>
+                <h3>Piirrettävän chartin raaka data</h3>
+            </div>
+
+            <div>
+                <b> Tänään on: {date} </b>
+            </div>
+
+            <div>
+                {rows()}
+            </div>
+
+            
         <h1> Lämpötila </h1>
    
       
@@ -40,7 +69,7 @@ function Weather() {
     ]}
 />
         <VictoryLine
-            data={[
+            data = {[
                 {experiment: "1.1", actual: -10},
                 {experiment: "2.1", actual: -5},
                 {experiment: "3.1", actual: 0},
@@ -48,6 +77,7 @@ function Weather() {
                 {experiment: "5.1", actual: 5},
                 {experiment: "6.1", actual: 12}
             ]}
+
             
         style={{data:
           
