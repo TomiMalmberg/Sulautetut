bfc3a66efe961eb72842ff5be603fd0d1076f6a8
TomiMalmberg <69836677+TomiMalmberg@users.noreply.github.com>
2020-09-17 11:48:39 +0300

    17.9.2020

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,33 +1,45 @@
 import React, { useState } from 'react'
-import {VictoryChart, VictoryLine, VictoryBar, VictoryTheme, VictoryScatter, VictoryVoronoiContainer} from 'victory'
+import { VictoryChart, VictoryLine, VictoryBar, VictoryTheme, VictoryScatter, VictoryVoronoiContainer } from 'victory'
 
 function Weather() {
-  
-    const data = [
-        {quarter: "1.1", earnings: "25"},
-        {quarter: "2.1", earnings: "35"},
-        {quarter: "3.1", earnings: "40"},
-        {quarter: "4.1", earnings: "45"},
-        {quarter: "5.1", earnings: "40"},
-        {quarter: "6.1", earnings: "55"}
 
-    ]
-    
+
     const today = new Date();
-    const date = today.getDate() + "." + parseInt(today.getMonth()+1) + "." + today.getFullYear();
+    const date = today.getDate() + "." + parseInt(today.getMonth() + 1) + "." + today.getFullYear();
 
     const initWeather = [];
     const [weather, setWeather] = useState(initWeather);
 
-    fetch('https://funcvariaiot.azurewebsites.net/api/HttpTriggerGetIotData?code=qO5qkShg0osHqY0BB2nfXI/anPgQ/K/3mIF7VTCFfaTdrvo6wl6DKw==')
-      .then(response => response.json())
-      .then(json => setWeather([...json]));
 
-      const rows = () => weather.slice(0, 24).reverse().map(temphum => {
-          const measurementDate = temphum.PublishedAt.split('T')[0].split('-')[2] + '.' + temphum.PublishedAt.split('T')[0].split('-')[1] + '.' + temphum.PublishedAt.split('T')[0].split('-')[0]
-          const measurementTime = temphum.PublishedAt.split('T')[1].split(':')[1] + ':' + temphum.PublishedAt.split('T')[1].split(':')[1]
-          return <div> <b>Pvm: </b> {measurementDate}, <b>klo:</b> {measurementTime}--------<b>Ilmankosteus:</b> {temphum.Hum.split('.')[0]}%--------<b>Lämpötila:</b> {temphum.Temp.split('.')[0]}</div>
-      })
+    fetch('https://funcvariaiot.azurewebsites.net/api/HttpTriggerGetIotData?code=qO5qkShg0osHqY0BB2nfXI/anPgQ/K/3mIF7VTCFfaTdrvo6wl6DKw==&amount=50')
+        .then(response => response.json())
+        .then(json => setWeather([...json]));
+
+
+    let chartTempDataDots = [];
+    let chartTempDataLine = [];
+    let charthumData = [];
+    let chartData = [];
+    const rows = () => weather.slice(0, 24).reverse().map(temphum => {
+        const measurementDate = temphum.PublishedAt.split('T')[0].split('-')[2] + '.' + temphum.PublishedAt.split('T')[0].split('-')[1] + '.' + temphum.PublishedAt.split('T')[0].split('-')[0]
+        const measurementTime = temphum.PublishedAt.split('T')[1].split(':')[1] + ':' + temphum.PublishedAt.split('T')[1].split(':')[1]
+        chartTempDataDots.push({ x: String(measurementTime), y: parseInt(temphum.Temp) });
+        chartTempDataLine.push({ experiment: String(measurementTime), actual: parseInt(temphum.Temp) });
+        charthumData.push({ xbar: String(measurementTime), ybar: parseInt(temphum.Hum)+"%" });
+        chartData.push({ quarter: String(measurementTime), earnings: parseInt(temphum.Hum) });
+        return <div> <b>Pvm: </b> {measurementDate}, <b>klo:</b> {measurementTime}--------<b>Ilmankosteus:</b> {temphum.Hum.split('.')[0]}%--------<b>Lämpötila:</b> {temphum.Temp.split('.')[0]}</div>
+    })
+
+    console.log(chartTempDataDots);
+    const showTempDataDots = chartTempDataDots;
+    console.log(chartTempDataLine);
+    const showTempDataLine = chartTempDataLine;
+    console.log(charthumData);
+    const showhumData = charthumData;
+    console.log(chartData);
+    const showData = chartData;
+
+    
 
     return (
         <div aling="center">
@@ -43,90 +55,75 @@ function Weather() {
                 {rows()}
             </div>
 
-            
-        <h1> Lämpötila </h1>
-   
-      
-        <VictoryChart
-        domainPadding={{x: 30, y:10}}
-            width={1000}
-            height={250}
-            
-     containerComponent={
-    <VictoryVoronoiContainer
-      mouseFollowTooltips
-      voronoiDimension="x"
-      labels={({ datum }) => `y: ${datum.y },
+
+            <h1> Lämpötila </h1>
+
+
+            <VictoryChart
+                domainPadding={{ x: 30, y: 10 }}
+                width={1000}
+                height={250}
+
+                containerComponent={
+                    <VictoryVoronoiContainer
+                        mouseFollowTooltips
+                        voronoiDimension="x"
+                        labels={({ datum }) => `y: ${datum.y},
       x: ${datum.x}`}
-    />
-  }
->
+                    />
+                }
+            >
 
-<VictoryScatter
-  	style={{ data: { fill: "red" }, labels: { fill: "red" } }}
-    data={[ 
-        {x: "1.1", y:-10}, {x: "2.1", y:-5}, {x: "3.1", y:0}, {x: "4.1", y:2}, {x: "5.1", y:5}, {x: "6.1", y: 12}
-    ]}
-/>
-        <VictoryLine
-            data = {[
-                {experiment: "1.1", actual: -10},
-                {experiment: "2.1", actual: -5},
-                {experiment: "3.1", actual: 0},
-                {experiment: "4.1", actual: 2},
-                {experiment: "5.1", actual: 5},
-                {experiment: "6.1", actual: 12}
-            ]}
-
-            
-        style={{data:
-          
-            {stroke: "blue", strokeWidth: 2}
-        }}
-        x="experiment"
-        y="actual"
-    />
+                <VictoryScatter
+                    style={{ data: { fill: "red" }, labels: { fill: "red" } }}
+                    data={showTempDataDots}
+                />
+                <VictoryLine
+                    data={showTempDataLine}
 
 
-    </VictoryChart>
-    
-        <div>
-            <h2>Ilmankosteus</h2>
+                    style={{
+                        data:
+
+                            { stroke: "blue", strokeWidth: 2 }
+                    }}
+                    x="experiment"
+                    y="actual"
+                />
+
+
+            </VictoryChart>
+
+            <div>
+                <h2>Ilmankosteus</h2>
+            </div>
+
+            <VictoryChart
+
+                const color="#969696"
+                theme={VictoryTheme.material}
+                domainPadding={{ x: 30, y: 10 }}
+                width={1000}
+                height={250}>
+
+
+                <VictoryBar
+                    data={showData}
+                    x="quarter"
+                    y="earnings"
+                />
+                <VictoryBar
+                    data={showhumData}
+                />
+
+            </VictoryChart>
         </div>
- 
-    <VictoryChart
 
-    const  color = "#969696"
-         theme={VictoryTheme.material}
-        domainPadding={{x: 30, y:10}}
-            width={1000}
-            height={250}>
-
-        
-            <VictoryBar
-            data={data}
-            x="quarter"
-            y="earnings"
-/>
-            <VictoryBar
-            data={[
-            {x: 1.1, y: 1,  label: "25%"},
-            {x: 2.1, y: 2,  label: "35%"},
-            {x: 3.1, y: 3,  label: "40%"},
-            {x: 4.1, y: 4,  label: "45%"},
-            {x: 5.1, y: 3,  label: "40%"},
-            {x: 6.1, y: 5,  label: "55%"},
-                      ]}
-        />
-
-        </VictoryChart>
-        </div>
-    
     )
-    
 
-        
-     
+
+
+
 }
 
 export default Weather;
